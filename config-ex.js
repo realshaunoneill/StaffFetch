e6b652f1108cc4c1fc6677fac2c297fa88af3810
Shaun O'Neill <shaunoneill7@me.com>
2018-06-19 00:06:11 +0100

    Finished and displays data

--- config-ex.js
+++ config-ex.js
@@ -1,6 +1,8 @@
 module.exports = {
     port: 80,
     guildID: "",
-    staffRoles: [],
+    ownerRoles: [],
+    adminRoles: [],
+    modRoles: [],
     discordToken: ""
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const config = require('../config');
+const quotes = require('../data/quotes');
 
 const Discord = require('discord.js');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const client = new Discord.Client();
 const app = express();
@@ -12,11 +15,34 @@ const app = express();
 let guild;
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}! Displaying ${config.staffRoles.length} roles publicly!`);
+    console.log(`Logged in as ${client.user.tag}! Displaying ${[...config.ownerRoles, ...config.adminRoles, ...config.modRoles].length} roles publicly!`);
 
     if (client.guilds.get(config.guildID)) {
         guild = client.guilds.get(config.guildID);
     } else return console.error(`This discord bot is not in the guild *${config.guildID}*! Exiting..`);
+});
+
+client.on('message', message => {
+    let allStaffRoles = [...config.ownerRoles, ...config.adminRoles, ...config.modRoles];
+
+    if (message.isMentioned(client.user)) return message.reply('Do ```/staffquote <Your quote>``` to save your quote for the website!').delete(1000).catch(err => {
+    });
+
+    if (!message.content.startsWith('/staffquote ')) return;
+
+    if (!message.member.roles.some(role => allStaffRoles.includes(role.id))) {
+        return message.author.send(`Sorry but you don't appear to have any of the following staff roles: \`\`\`${allStaffRoles}\`\`\``).catch(err => {
+        });
+    }
+
+    let msgSplit = message.content.split(' ').slice(1);
+
+    let authorID = message.author.id;
+    quotes[authorID] = msgSplit.join(' ');
+
+    console.info(`Saved new quote for *${message.author.tag}* - ${msgSplit.join(' ')}`);
+
+    saveQuotesToFile();
 });
 
 app.use(bodyParser.json());
@@ -25,12 +51,48 @@ app.use(cors());
 
 app.get('*', (req, res) => {
     try {
-        let results = {};
 
-        config.staffRoles.forEach(roleID => {
+        let owners = [];
+        let admins = [];
+        let mods = [];
+
+        config.ownerRoles.forEach(roleID => {
             let role = guild.roles.get(roleID);
-            results[role.name] = role.members.map(m => m.tag);
+            owners = [...owners, ...role.members.map(m => {
+                return {
+                    id: m.user.id,
+                    username: m.user.tag,
+                    picture: m.user.displayAvatarURL,
+                    quote: quotes[m.user.id] || `None supplied!`
+                }
+            })];
         });
+
+        config.adminRoles.forEach(roleID => {
+            let role = guild.roles.get(roleID);
+            admins = [...admins, role.members.map(m => {
+                return {
+                    id: m.user.id,
+                    username: m.user.tag,
+                    picture: m.user.displayAvatarURL,
+                    quote: quotes[m.user.id] || `None supplied!`
+                }
+            })];
+        });
+
+        config.modRoles.forEach(roleID => {
+            let role = guild.roles.get(roleID);
+            mods = [...mods, ...role.members.map(m => {
+                return {
+                    id: m.user.id,
+                    username: m.user.tag,
+                    picture: m.user.displayAvatarURL,
+                    quote: quotes[m.user.id] || `None supplied!`
+                }
+            })];
+        });
+
+        let results = {owner: owners, admins: admins, mods: mods};
 
         res.status(200).json(results);
 
@@ -46,10 +108,18 @@ if (config.discordToken) client.login(config.discordToken).catch(err => {
 
 // Final webserver
 const httpServer = http.createServer(app);
-let port = exports.port;
+let port = config.port;
 httpServer.listen(port, (err) => {
     if (err) {
         return console.error(`FAILED TO OPEN WEB SERVER, ERROR: ${err.stack}`);
     }
     console.info(`Successfully started webserver... listening on port: ${config.port}`);
 });
+
+function saveQuotesToFile() {
+    try {
+        fs.writeFileSync(path.join(__dirname, '../data/quotes.json'), JSON.stringify(quotes));
+    } catch (err) {
+        console.error(`Error saving quotes to file, Error: ${err.stack}`);
+    }
+}
